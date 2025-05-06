4968a6bd5accbfa1155951c0de105f9d139ab37e
Jules Beausaert <julesbeausaert@fullname.local>
2025-05-06 13:24:46 +0200

    update

---
+++ FlipWise/FlipWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlipWiseApp.swift
+//  FlipWise
+//
+//  Created by Jules Beausaert on 06/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct FlipWiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
